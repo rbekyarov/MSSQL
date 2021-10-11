4f6ea8cfb67eecfa0695407c392c2df45e124ce2
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-11 09:54:03 +0300

    Exercise Intro

--- src/MSSQL_02_Exercise_Databases_Introduction/lab02.sql
+++ src/MSSQL_02_Exercise_Databases_Introduction/lab02.sql
@@ -2,10 +2,10 @@ USE Minions
 CREATE TABLE Minions (
     Id INT PRIMARY KEY ,
     Name VARCHAR(40),
-    Age INT NOT NULL
+    Age INT
 );
 CREATE TABLE Towns (
     Id INT PRIMARY KEY,
     Name VARCHAR(40),
 
-)
+);
